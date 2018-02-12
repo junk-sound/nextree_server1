61ace53fd6c12d2b9c1e04bcd7aad42473c932f4
junksound <wom2277@naver.com>
2018-02-12 15:13:47 +0900

    set post, connext nginx 1st

--- nextree_server/settings/prod.py
+++ nextree_server/settings/prod.py
@@ -7,9 +7,11 @@ ALLOWED_HOSTS = ['\*']
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
 
-DATABASES = { 'default': {
-  'ENGINE': 'django.db.backends.postgresql', 'NAME': 'ubuntu',
-  'USER': 'ubuntu',
-  'PASSWORD': 'password',
-  'HOST': '127.0.0.1', },
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'junkdb',
+        'USER': 'junksound',
+        'PASSWORD': 'wekimeki',
+        'HOST': '127.0.0.1', },
 }
