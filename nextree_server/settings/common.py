2f2055226b63dfd4050be6d7fe80024d73679c8d
junksound <wom2277@naver.com>
2018-02-12 22:58:00 +0900

    basicauthentification

--- nextree_server/settings/common.py
+++ nextree_server/settings/common.py
@@ -135,9 +135,10 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework.authentication.SessionAuthentication',
+
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ),
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.AllowAny',
